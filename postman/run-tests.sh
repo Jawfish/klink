9aa704b8641cbd95845ac8fba667421e4ef36593
James Fitzgerald <kaphix@gmail.com>
2023-07-26 19:23:41 -0400

    feat: initial postman integration testing

---
+++ postman/run-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+wait_for_service() {
+    echo "Waiting for service $1 to be ready..."
+    while true; do
+        curl --silent $1 > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Service $1 is ready."
+            break
+        else
+            echo "Service $1 cannot be reached. Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+}
+
+wait_for_service ${AUTH_SERVICE_URL}
+wait_for_service ${USER_SERVICE_URL}
+wait_for_service ${POST_SERVICE_URL}
+
+newman run collection.json \
+    --env-var "AUTH_SERVICE_URL=${AUTH_SERVICE_URL}" \
+    --env-var "USER_SERVICE_URL=${USER_SERVICE_URL}" \
+    --env-var "POST_SERVICE_URL=${POST_SERVICE_URL}"
